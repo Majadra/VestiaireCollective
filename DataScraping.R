731ecb63fd1326731b0148c88611926013e0dbb9
Majadra <Ran_Kirshenboim@walla.com>
2020-08-08 15:46:54 +0200

    08/08 update

--- DataScraping.R
+++ DataScraping.R
@@ -1,74 +1,72 @@
 library(RSelenium)
 library(rvest)
+library(stringr)
+
+##### Preparing the server
 remDr <- remoteDriver(remoteServerAddr = "192.168.99.100", port = 4445L, browser = "chrome")
 remDr$open()
 
-
+##### Navigating to login.
 remDr$navigate("https://us.vestiairecollective.com/")
 main_webpage <- remDr$findElement("css", "body")
 Login_button <- remDr$findElement(using = "css", ".d-lg-none+ .d-lg-block .resetButton")
 main_webpage$mouseMoveToLocation(webElement = Login_button) 
 main_webpage$click(1)
-##### Here I will connect to the profile
+
+##### Here I will connecting to profile
 ##### After connecting we will navigate into to heels section
+
+Email <- remDr$findElement(using = "css", "#user_email")
+Email
 
 remDr$navigate("https://us.vestiairecollective.com/women-shoes/heels/")
 main_webpage <- remDr$findElement("css", "body")
 
-Online_Since <- list()
-Designer <- list()
-Model <- list()
-Price <- list()
-Condition <- list()
-Size <- list()
-Location <- list()
-Reference <- list()
-Material <- list()
-Color <- list()
-load_variable <- function(remDr,variable,address) {
-  remDr$findElement(using = "css", as.character(address)) 
+GoToSleep <- function() {
+  Sys.sleep(runif(1,3,4)) 
 }
+
+# Initialising  the variables we will collect.
+Price <- list()
+Likes <- list()
+Other_Details <- list() # all other information (will be stored in a vector)
 
 for (i in 1:60)
 {
   Temp_individual_item <- remDr$findElement(using = "css", paste0(".col-lg-4:nth-child(",i,") .productSnippet__imageWrapper")) 
   main_webpage$mouseMoveToLocation(webElement = Temp_individual_item) 
-  main_webpage$click(1)
-  Sys.sleep(3)
-  # All the detailes will be taken from for the individual pages
-  Temp_Online_since <- remDr$findElement(using = "css", ".descriptionList__block__list li:nth-child(1)") 
-  Temp_Designer <- remDr$findElement(using = "css", ".descriptionList__block__list li:nth-child(5)")
-  Temp_Model <- remDr$findElement(using = "css", ".descriptionList__block__list li:nth-child(6)")
+  main_webpage$click(1) #Clicking a link to view a specific shoe.
+  GoToSleep()
+  
+  #
+  Temp_Other_Details <- remDr$findElements(using = "css", ".descriptionList__block__list li") 
+  Temp_Likes <- remDr$findElement(using = "css", ".productTitle__like")
   Temp_Price <- remDr$findElement(using = "css", ".productPrice__price")
-  Temp_Condition <- remDr$findElement(using = "css", ".descriptionList__block__list span")
-  Temp_Size <- remDr$findElement(using = "css", ".descriptionList__block__list li:nth-child(10)")
-  try(Temp_Location <- remDr$findElement(using = "css", ".descriptionList__block__list li:nth-child(11)"),silent = T)
-  try(Temp_Reference <- remDr$findElement(using = "css", ".descriptionList__block__list li:nth-child(12)"),silent = T)
-  Temp_Material <- remDr$findElement(using = "css", ".descriptionList__block__list li:nth-child(8)")
-  Temp_Color <- remDr$findElement(using = "css", ".descriptionList__block__list li:nth-child(9)")
 
-  Online_Since[i] <- Temp_Online_since$getElementText()
-  Designer[i] <- Temp_Designer$getElementText()
-  Model[i] <- Temp_Model$getElementText()
+  Other_Details[[i]] <- sapply(Temp_Other_Details,function(x){x$getElementText()[[1]]})
+  Likes[i] <- Temp_Likes$getElementText()
   Price[i] <- Temp_Price$getElementText()
-  Condition[i] <- Temp_Size$getElementText()
-  Size[i] <- Temp_Size$getElementText()
-  try(Location[i] <- Temp_Location$getElementText(),silent = T)
-  try(Reference[i] <- Temp_Reference$getElementText(),silent = T)
-  Material[i] <- Temp_Material$getElementText()
-  Color[i] <- Temp_Color$getElementText()
 
-main_webpage$goBack() 
-Sys.sleep(3)
+main_webpage$goBack()  #Returning to the main webpage.
+GoToSleep()
 print(paste0("scraping product number - ",i))
   }
 
 
-### Thats the link .descriptionList__column:nth-child(1) li
-Test <- remDr$findElement(using = "css", ".descriptionList__column:nth-child(1) li")
-Test$getElementText()
-main_webpage$mouseMoveToLocation(webElement = Temp_individual_item) 
-main_webpage$click(1)
-Test
-HeelsPage1 <- main_webpage$getPageSource() %>% .[[1]] %>% read_html()
+
+
+
+
+
+
+#### Testing zone
+Test <- 
+  remDr$findElements(using = "css", "a")
+Test$findChildElement()
+Test$getElementAttribute('href')
+linkList <- sapply(Test,function(x){x$getElementAttribute('href')})
+linkList <- as.vector(unlist(linkList))
+grepl("^https://us.vestiairecollective.com/women-shoes/heels.+-[[:digit:]]{3,}.shtml",linkList[380])
+onlygoodlinks <- 
+  linkList[which(grepl("^https://us.vestiairecollective.com/women-shoes/heels.+-[[:digit:]]{3,}.shtml",linkList) == TRUE)]
 
